ae3062a0eb2f096c44f722fa4c1de89f924f2aed
DOMENICO CALABRIA <domenico.calabria@student.supsi.ch>
2019-04-25 13:16:32 +0200

    Aggiunto una nuova UI più dettagliata su base Griglia e con la capacità di modificare valori Ancora da aggiungere le icone da visualizzare

--- MeteoApp/Views/MeteoItemPage.xaml.cs
+++ MeteoApp/Views/MeteoItemPage.xaml.cs
@@ -1,12 +1,26 @@
-﻿using Xamarin.Forms;
+﻿using Java.Lang;
+using Xamarin.Forms;
 
 namespace MeteoApp
 {
     public partial class MeteoItemPage : ContentPage
     {
+        public string ForecastIcon { get; set; }
+        public string NomeCitta { get; set; }
+        public string Temp { get; set; }
+        public string MaxTemp { get; set; }
+        public string MinTemp { get; set; }
+        public string ForecastExtended { get; set; }
+
         public MeteoItemPage()
         {
             InitializeComponent();
+            ForecastIcon = "sunny.png";
+            NomeCitta = "L'isola che non c'é";
+            Temp = Integer.ToString(30);
+            MaxTemp = Integer.ToString(45);
+            MinTemp = Integer.ToString(15);
+            ForecastExtended = "Parzialmente nuvoloso con tracce di sole e neve";
         }
     }
 }
