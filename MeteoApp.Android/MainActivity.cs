37d296cbec4424e61e1f7a61e50e62d88a1b6924
nimisha manjali <nimisha.manjali@student.supsi.ch>
2019-05-02 22:14:57 +0200

    merged with dev but not in dev

--- MeteoApp.Android/MainActivity.cs
+++ MeteoApp.Android/MainActivity.cs
@@ -7,7 +7,7 @@ using Android.Views;
 using Android.Widget;
 using Android.OS;
 using Acr.UserDialogs;
-
+using Xamarin.Forms.Maps;
 namespace MeteoApp.Droid
 {
     [Activity(Label = "MeteoApp", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
@@ -15,6 +15,7 @@ namespace MeteoApp.Droid
     {
         protected override void OnCreate(Bundle savedInstanceState)
         {
+            Plugin.CurrentActivity.CrossCurrentActivity.Current.Init(this, savedInstanceState);
             TabLayoutResource = Resource.Layout.Tabbar;
             ToolbarResource = Resource.Layout.Toolbar;
 
@@ -22,6 +23,13 @@ namespace MeteoApp.Droid
             global::Xamarin.Forms.Forms.Init(this, savedInstanceState);
             LoadApplication(new App());
             UserDialogs.Init(this);
+            Xamarin.FormsMaps.Init(this, savedInstanceState);
+        }
+        public override void OnRequestPermissionsResult(int requestCode, string[] permissions, Android.Content.PM.Permission[] grantResults)
+        {
+            Plugin.Permissions.PermissionsImplementation.Current.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+            base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
         }
     }
+    
 }
--- MeteoApp/Views/MeteoItemPage.xaml.cs
+++ MeteoApp/Views/MeteoItemPage.xaml.cs
@@ -1,19 +1,61 @@
-﻿
-using System;
-using System.Net.Http;
-using MeteoApp.Models;
-using Newtonsoft.Json;
+﻿using MeteoApp.Models;
 using Xamarin.Forms;
+using Plugin.Geolocator;
 
+using System;
+using System.Diagnostics;
+using Xamarin.Forms.Maps;
 namespace MeteoApp
 {
     public partial class MeteoItemPage : ContentPage
     {
+        private Location l;
+        Geocoder geocoder;
         public MeteoItemPage()
         {
             InitializeComponent();
-           
+            GetLocation();
+            l = new Location();
+            //BindingContext = new MeteoItemViewModel(l);
+            geocoder = new Geocoder();
+
         }
+
+
+
+        async void GetLocation()
+        {
+            var locator = CrossGeolocator.Current;
+            var position = await locator.GetPositionAsync(TimeSpan.FromSeconds(10));
+            l.Latitude = position.Latitude;
+            l.Longitude = position.Longitude;
+
+            Debug.WriteLine("Position Status: {0}", position.Timestamp);
+            Debug.WriteLine("Position Latitude: {0}", position.Latitude);
+            Debug.WriteLine("Position Longitude: {0}", position.Longitude);
+            Debug.WriteLine("ID: {0}", l.ID);
+            var positions = new Position(position.Latitude, position.Longitude);
+
+
+            var possibleAddresses = await geocoder.GetAddressesForPositionAsync(positions);
+
+            foreach (var address in possibleAddresses)
+            {
+                Debug.WriteLine(address);
+                l.Name = address;
+                Debug.WriteLine("name is" + l.Name);
+
+            }
+
+
+            Debug.WriteLine("nameout is" + l.Name);
+
+
+
+
+        }
+
+
 
     }
 }
