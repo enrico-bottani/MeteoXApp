8680b9daa1d5ecfe78a0cdf5c8b587d3f686298c
Enrico Bottani <enrico.bottani@hotmail.com>
2019-05-02 18:19:14 +0200

    Request from OpenWeather API and JSON parser

---
+++ MeteoApp/Models/OpenWeatherClasses/Coord.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace MeteoApp.Models.OpenWeatherClasses
+{
+    public class Coord
+    {
+        public double lon,lat;
+        public Coord()
+        {
+
+        }
+    }
+}
---
+++ MeteoApp/Models/OpenWeatherClasses/Weather.cs
@@ -0,0 +1,13 @@
+﻿using System;
+namespace MeteoApp.Models.OpenWeatherClasses
+{
+    public class Weather
+    {
+        public int id;
+        public string main,description, icon;
+        public Weather()
+        {
+           
+        }
+    }
+}
