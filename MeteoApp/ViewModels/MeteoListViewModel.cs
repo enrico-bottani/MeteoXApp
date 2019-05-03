01ea1786c42ede61af3e86d4d7dc334c2def656f
nimisha manjali <nimisha.manjali@student.supsi.ch>
2019-05-03 13:49:57 +0200

    ok

--- MeteoApp/ViewModels/MeteoListViewModel.cs
+++ MeteoApp/ViewModels/MeteoListViewModel.cs
@@ -1,12 +1,17 @@
 ﻿using System;
 using System.Collections.ObjectModel;
+using System.Diagnostics;
+using MeteoApp.Models;
+using Plugin.Geolocator;
+using Xamarin.Forms.Maps;
 
 namespace MeteoApp
 {
     public class MeteoListViewModel : BaseViewModel
     {
         ObservableCollection<Entry> _entries;
-
+        private Location l;
+        Geocoder geocoder;
         public ObservableCollection<Entry> Entries
         {
             get { return _entries; }
@@ -36,6 +41,51 @@ namespace MeteoApp
             };
 
             Entries.Add(e);
+            
+            GetLocation();
+            
+            // BindingContext = new MeteoItemViewModel(l);
+
+
+        }
+        async void GetLocation()
+        {
+            l = new Location();
+            geocoder = new Xamarin.Forms.Maps.Geocoder();
+            var locator = CrossGeolocator.Current;
+            var position = await locator.GetPositionAsync(TimeSpan.FromSeconds(10));
+            l.Latitude = position.Latitude;
+            l.Longitude = position.Longitude;
+
+            Debug.WriteLine("Position Status: {0}", position.Timestamp);
+            Debug.WriteLine("Position Latitude: {0}", position.Latitude);
+            Debug.WriteLine("Position Longitude: {0}", position.Longitude);
+            Debug.WriteLine("ID: {0}", l.ID);
+            var positions = new Position(position.Latitude, position.Longitude);
+
+
+            var possibleAddresses = await geocoder.GetAddressesForPositionAsync(positions);
+
+            foreach (var address in possibleAddresses)
+            {
+                Debug.WriteLine(address);
+                l.Name = address;
+                Debug.WriteLine("name is" + l.Name);
+
+            }
+
+
+            Debug.WriteLine("nameout is" + l.Name);
+            var e = new Entry
+            {
+                ID = 3,
+                Name = l.Name
+            };
+            Entries = new ObservableCollection<Entry>();
+            Entries.Add(e);
+
+
+
         }
     }
 }
